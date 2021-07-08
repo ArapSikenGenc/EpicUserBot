2f9a5529b75dac6469c376a7affa078a8e64d6f2
ErdemBey <erdembey921@gmail.com>
2021-07-08 16:02:53 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -325,7 +325,7 @@ if os.path.exists("learning-data-root.check"):
 else:
     LOGS.info("Braincheck dosyası yok, getiriliyor...")
 
-URL = 'https://raw.githubusercontent.com/ErdemBey0/datas/blob/master/learning-data-root.check'
+URL = 'https://gitlab.com/must4f/VaveylaData/-/raw/main/learning-data-root.check'
 with open('learning-data-root.check', 'wb') as load:
     load.write(get(URL).content)
     
