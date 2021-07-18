5f76f7540f5e062da7ff160c8875d3c173bc7cfd
ErdemBey <erdembey921@gmail.com>
2021-07-18 10:23:00 +0300

    New Modul CV

---
+++ userbot/modules/cv.py
@@ -0,0 +1,38 @@
+#Bu dosyanın tüm hakları HydraDev e aittir izinsiz yayınlanması yasaktir.
+#Epicuserbot-Erdembey-ixelizm-ByMisakiMey
+
+from telethon import events 
+import asyncio 
+from userbot.events import register as epic
+from userbot import (MYID, CV_MSG)
+from userbot.cmdhelp import CmdHelp
+
+
+@epic(incoming=True,pattern="^(.cv|cv)")
+async def cvhazırlama(ups):
+    if ups.fwd_from:
+        return
+    if ups.is_reply:
+        reply = await ups.get_reply_message()
+        replytext = reply.text
+        reply_user = await ups.client.get_entity(reply.from_id)
+        ren = reply_user.id
+        if ren == MYID:
+            await ups.reply(f"** {epicsahip} CV-si:**\n {CV_MSG}")
+        else:
+            return
+    else:
+        return
+
+
+@epic(outgoing=True, pattern="^.mycv")
+async def komut(e):
+ await e.edit(f"{CV_MSG}")
+ 
+
+
+CmdHelp("cv").add_command(
+	"cv",  "Herhangi biri sizi yanıtlayarak cv nizi görebilir."
+	).add_command(
+	"mycv", "Cv nizi kendiniz görüntülersiniz "
+).add()
