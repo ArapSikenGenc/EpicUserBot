067a25b83c9ae2ec6fefee736e5f94f546471eed
ErdemBey <erdembey921@gmail.com>
2021-07-18 09:29:43 +0300

    Add files via upload

---
+++ userbot/modules/emall.py
@@ -0,0 +1,60 @@
+# EpicUsebot ErdemBey / Bymisakimey
+
+import random
+import asyncio
+from userbot.events import register 
+from userbot import CMD_HELP, bot
+from userbot import EPİC_VERSION
+from telethon.tl.types import ChannelParticipantsAdmins as cp
+from time import sleep
+from userbot.cmdhelp import CmdHelp
+
+
+emoji = "🐵 🦁 🐯 🐱 🐶 🐺 🐻 🐨 🐼 🐹 🐭 🐰 🦊 🦝 🐮 🐷 🐽 🐗 🦓 🦄 🐴 🐸 🐲 🦎 🐉 🦖 🦕 🐢 🐊 🐍 🐁 🐀 🐇 🐈 🐩 🐕 🦮 🐕‍🦺 🐅 🐆 🐎 🐖 🐄 🐂 🐃 🐏 🐑 🐐 🦌 🦙 🦥 🦘 🐘 🦏 🦛 🦒 🐒 🦍 🦧 🐪 🐫 🐿️ 🦨 🦡 🦔 🦦 🦇 🐓 🐔 🐣 🐤 🐥 🐦 🦉 🦅 🦜 🕊️ 🦢 🦩 🦚 🦃 🦆 🐧🦈🐬🐋🐳🐟🐠🐡🦐🦞 🦀 🦑 🐙 🦪 🦂 🕷️ 🦋 🐞 🐝 🦟 🦗 🐜 🐌 🐚 🕸️ 🐛 🐾 😀 😃 😄 😁 😆 😅 😂 🤣 😭 😗 😙 😚 😘 🥰 😍 🤩 🥳 🤗 🙃 🙂 ☺️ 😊 😏 😌 😉 🤭 😶 😐 😑 😔 😋 😛 😝 😜 🤪 🤔 🤨 🧐 🙄 😒 😤 😠 🤬 ☹️ 🙁 😕 😟 🥺 😳 😬 🤐 🤫 😰 😨 😧 😦 😮 😯 😲 😱 🤯 😢 😥 😓 😞 😖 😣 😩 😫 🤤 🥱 😴 😪 🌛 🌜 🌚 🌝 🌞 🤢 🤮 🤧 🤒 🍓 🍒 🍎 🍉 🍑 🍊 🥭 🍍 🍌 🌶 🍇 🥝 🍐 🍏 🍈 🍋 🍄 🥕 🍠 🧅 🌽 🥦 🥒 🥬 🥑 🥯 🥖 🥐 🍞 🥜 🌰 🥔 🧄 🍆 🧇 🥞 🥚 🧀 🥓 🥩 🍗 🍖 🥙 🌯 🌮 🍕 🍟 🥨 🥪 🌭 🍔 🧆 🥘 🍝 🥫 🥣 🥗 🍲 🍛 🍜 🍢 🥟 🍱 🍚 🥡 🍤 🍣 🦞 🦪 🍘 🍡 🥠 🥮 🍧 🍧 🍨".split(" ")
+
+
+class FlagContainer:
+    is_active = False
+
+
+
+@register(outgoing=True, pattern="^.emall.*")
+async def b(event):
+    if event.fwd_from or FlagContainer.is_active:
+        return
+    try:
+        FlagContainer.is_active = True
+
+        text = None
+        args = event.message.text.split(" ", 1)
+        if len(args) > 1:
+            text = args[1]
+
+        chat = await event.get_input_chat()
+        await event.delete()
+
+        tags = list(map(lambda m: f"[{random.choice(emoji)}](tg://user?id={m.id})", await event.client.get_participants(chat)))
+        current_pack = []
+        async for participant in event.client.iter_participants(chat):
+            if not FlagContainer.is_active:
+                break
+
+            current_pack.append(participant)
+
+            if len(current_pack) == 5:
+                tags = list(map(lambda m: f"[{random.choice(emoji)}](tg://user?id={m.id})", current_pack))
+                current_pack = []
+
+                if text:
+                    tags.append(text)
+
+                await event.client.send_message(event.chat_id, " ".join(tags))
+                await asyncio.sleep(0.5)
+    finally:
+        FlagContainer.is_active = False
+
+CmdHelp("emall").add_command(
+	"emall", "<sebep>", "Gruptaki Üyeleri Emojili Bir Şekilde Etiketler."
+    ).add_command(
+    "kill all", None, "Etiketleme işlemini durdurur."
+).add()
