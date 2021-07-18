88e54dea54c38976977c87b943c37ce4c5155d6a
ErdemBey <erdewbey@gmail.com>
2021-07-18 14:27:53 +0300

    new cv version

--- userbot/modules/cv.py
+++ userbot/modules/cv.py
@@ -4,11 +4,11 @@
 from telethon import events 
 import asyncio 
 from userbot.events import register as epic
-from userbot import (MYID, CV_MSG, DEFAULT_NAME)
+from userbot import (MYID)
+from userbot.main import PLUGIN_MESAJLAR
 from userbot.cmdhelp import CmdHelp
 
-
-@epic(incoming=True,pattern="^(.cv|cv)")
+@epic(incoming=True, pattern="^.cv")
 async def cvhazırlama(ups):
     if ups.fwd_from:
         return
@@ -18,23 +18,16 @@ async def cvhazırlama(ups):
         reply_user = await ups.client.get_entity(reply.from_id)
         ren = reply_user.id
         if ren == MYID:
-            await ups.reply(f"** {DEFAULT_NAME} CV-si:**\n {CV_MSG}")
-        else:
-            return
-    else:
-        return
-
-
-@epic(outgoing=True, pattern="^.mycv") #Kendi CV-mizi Görmek İçin
+            await ups.reply(f"{PLUGIN_MESAJLAR['cv']}")
+		        
+@epic(outgoing=True, pattern="^.mycv")
 async def komut(e):
- await e.edit(f"{CV_MSG}")
- 
-
+        await e.edit(f"{PLUGIN_MESAJLAR['cv']}")
 
 CmdHelp("cv").add_command(
 	"cv",  "Herhangi biri sizi yanıtlayarak cv nizi görebilir."
 	).add_command(
 	"mycv", "Cv nizi kendiniz görüntülersiniz "
 	).add_command(
-        ".set var CV_MSG", "CV Mesajın"
+	".değiştir cv", "cv-nizi değiştirmek için"
 ).add()
