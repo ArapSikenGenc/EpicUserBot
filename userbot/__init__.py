09e7c8d030a531fd81e8b8b1ba2ccd5958715771
Misaki <73439036+ByMisakiMey@users.noreply.github.com>
2021-09-18 13:51:34 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -75,7 +75,7 @@ if not LANGUAGE in ["EN", "TR", "AZ", "UZ", "DEFAULT"]:
     LANGUAGE = "DEFAULT"
     
 # Epic versiyon
-EPİC_VERSION = "v31"
+EPİC_VERSION = "v3.5"
 
 MAX_MESSAGE_SIZE_LIMIT = 4095
 # Telegram API KEY ve HASH
