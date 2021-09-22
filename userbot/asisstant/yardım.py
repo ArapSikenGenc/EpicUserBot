9ca3952744031b94f617aad62e7fab0e719f6df0
Misaki <73439036+ByMisakiMey@users.noreply.github.com>
2021-09-22 22:23:08 +0300

    Create yardım.py

---
+++ userbot/asisstant/yardım.py
@@ -0,0 +1,24 @@
+class Text:
+    nece = "`Bir mp3 faylına cavab verin və ya bir mahnı adı qeyd edin!`"
+    admin_deyilsen = "`Bunu etmək üçün icazəniz yoxdur!`"
+    yukle = "`Yüklənir...`"
+    uploading = "`Hazırlanır..`"
+    yardim = """
+**Əmrlər:**\n
+  - `{x}alive` - __asistanın aktiv olub olmadığını yoxlayın.__
+  - `{x}seslendir <link/mp3-ə cavab>` - __musiqini səsləndirər.__
+  - `{x}pauza` - __pauza edər.__
+  - `{x}resume` - __musiqini davam etdirər.__
+  - `{x}song <mahnı adı>` - __musiqini yükləyər.__
+**EPİC USERBOT**"""
+
+
+# class Extra:
+
+    
+   
+async def mahniniseslendir(pycalls, message, song):
+    try:
+        await pycalls.stream(message.chat.id, song)
+    except Exception as e:
+        await message.reply_text(f"Səsləndirilə bilmədi!\n\nXəta:\n{e}")    
