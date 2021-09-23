17c41670930418a154d347d9b02a366d646bb44c
Misaki <73439036+ByMisakiMey@users.noreply.github.com>
2021-09-23 20:57:33 +0300

    Update ping.py

--- userbot/asisstant/ping.py
+++ userbot/asisstant/ping.py
@@ -10,9 +10,10 @@ from datetime import datetime
 from userbot import tgbot, bot
 from telethon import events
 from userbot import tgbot, OWNER_ID
+from userbot.utils.events import register
 import asyncio
 
-@tgbot.on(events.NewMessage(incoming=True, pattern="deneme"))
+@register(incoming=True, pattern="deneme")
 async def evnt (e):
  if evnt.sender_id in OWNER_ID:
     start = datetime.now()
