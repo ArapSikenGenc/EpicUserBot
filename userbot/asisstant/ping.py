2705b748b4f46e3cac7efbf4534075a18ec5d46b
Misaki <73439036+ByMisakiMey@users.noreply.github.com>
2021-09-23 21:30:11 +0300

    Update ping.py

--- userbot/asisstant/ping.py
+++ userbot/asisstant/ping.py
@@ -16,7 +16,7 @@ import asyncio
 @register(incoming=True, from_users=OWNER_ID, pattern="^deneme$")
 async def evnt (e):
     start = datetime.now()
-    msg = await evnt.reply("Pong!")
+    msg = await evnt.respond("Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await msg.edit(f"**Pong!!**\n `{ms} ms`")
